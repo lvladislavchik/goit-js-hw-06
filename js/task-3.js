2afd77572c34a328e1c569334a44727f841426be
lvladislavchik <lvladislavchik@ukr.net>
2024-01-30 18:38:41 +0200

    completed

--- js/task-3.js
+++ js/task-3.js
@@ -1,15 +1,47 @@
-function textConvert(value) {
-  return Number.parseFloat(value.replace(',', '.'));
+'use strict';
+
+// Напиши клас StringBuilder, який приймає один параметр initialValue — довільний рядок, який записується у
+// приватну властивість value об'єкта, що створюється.
+
+// Оголоси наступні методи класу:
+
+// getValue() — повертає поточне значення приватної властивості value.
+// padEnd(str) — отримує параметр str (рядок) і додає його в кінець значення приватної властивості value об'єкта,
+// який викликає цей метод.
+// padStart(str) — отримує параметр str (рядок) і додає його на початок значення приватної властивості value
+// об'єкта, який викликає цей метод.
+// padBoth(str) — отримує параметр str (рядок) і додає його на початок і в кінець значення приватної властивості
+// value об'єкта, який викликає цей метод.
+
+class StringBuilder {
+  #value;
+  
+  constructor(initialValue) {
+    this.#value = initialValue;
+  }
+
+  getValue(){
+    return this.#value
+  }
+
+  padEnd(str){
+    this.#value = `${this.#value}${str}`
+  }
+
+  padStart(str) {
+    this.#value = `${str}${this.#value}`
+  }
+
+  padBoth(str){
+    this.#value = `${str}${this.#value}${str}`
+  }
 }
 
-function getElementWidth(content, padding, border) {
-  content = textConvert(content);
-  padding = textConvert(padding);
-  border = textConvert(border);
-  const elementWidth = content + padding * 2 + border * 2;
-  return elementWidth;
-}
-
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
